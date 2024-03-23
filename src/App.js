280addde585ca9d42a4542a447bf2ba77a1950c5
schance14 <schance.sd14@gmail.com>
2024-03-23 12:13:00 -0600

    import NumberOfEvents to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
 
 const App = () => {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <CitySearch />
+      <NumberOfEvents />
     </div>
   );
 };
