c03f5bfb75819591a0c23b24603ba312f1bd58ba
schance14 <schance.sd14@gmail.com>
2024-03-26 21:56:11 -0600

    app integration testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { extractLocations, getEvents } from "./api";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  // populate the events state with the events list you’ll fetch
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
