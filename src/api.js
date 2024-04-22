1ff2967b8d6dcbc476b8518e70902d8ed38b3834
schance14 <schance.sd14@gmail.com>
2024-04-21 19:28:58 -0600

    update to implement online/offline functionality

--- src/api.js
+++ src/api.js
@@ -14,7 +14,6 @@ export const getEvents = async () => {
   }
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
   const token = await getAccessToken();
@@ -28,7 +27,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
